c87686f956089157141897cf556f5ce5c87f8148
lottenoorlander <lottenoorlander@gmail.com>
2019-12-13 11:04:14 +0100

    changed router to async await syntx

--- message/router.js
+++ message/router.js
@@ -4,16 +4,14 @@ const Message = require("./model");
 
 const router = new Router();
 
-router.get("/message", (req, res, next) => {
-  Message.findAll()
-    .then(messages => res.send(messages))
-    .catch(error => next(error));
+router.get("/message", async (req, res, next) => {
+  const messages = await Message.findAll();
+  res.send(messages);
 });
 
-router.post("/message", (req, res, next) => {
-  Message.create(req.body)
-    .then(message => res.send(message))
-    .catch(error => next(error));
+router.post("/message", async (req, res, next) => {
+  const message = await Message.create(req.body);
+  res.send(message);
 });
 
 module.exports = router;
