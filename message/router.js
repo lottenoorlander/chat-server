c32179428e3d36625723fc48175f09b12fe119f5
lottenoorlander <lottenoorlander@gmail.com>
2019-12-13 11:22:52 +0100

    added async error handling

--- message/router.js
+++ message/router.js
@@ -5,13 +5,21 @@ const Message = require("./model");
 const router = new Router();
 
 router.get("/message", async (req, res, next) => {
-  const messages = await Message.findAll();
-  res.send(messages);
+  try {
+    const messages = await Message.findAll();
+    res.send(messages);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/message", async (req, res, next) => {
-  const message = await Message.create(req.body);
-  res.send(message);
+  try {
+    const message = await Message.create(req.body);
+    res.send(message);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
